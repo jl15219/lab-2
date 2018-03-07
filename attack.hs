0d220b6f2dcde257e13f98840f862d8b306738d6
Jamie Luxford <jl15219@my.bristol.ac.uk>
2018-03-07 10:48:25 +0000

    So slow

--- attack.hs
+++ attack.hs
@@ -3,7 +3,7 @@ import System.Process
 
 data FalseTuple a = F a a
 
-givenArray = ['a'..'z'] ++ ['A'..'Z']
+givenArray = ['a'..'z'] -- ++ ['A'..'Z']
 
 forceFalseTuple :: FalseTuple a -> FalseTuple a
 forceFalseTuple = id
@@ -14,6 +14,7 @@ instance Show a => Show (FalseTuple a) where
 readFT :: [Char] -> FalseTuple Integer
 readFT = readFT' [] [] where
     readFT' :: [Char] -> [Char] -> [Char] -> FalseTuple Integer
+    readFT' a b [] = F ((read.reverse) a) ((read.reverse) b)
     readFT' a [] ('\n':c:rs) = readFT' a [c] rs
     readFT' a [] (s:as)      = readFT' (s:a) [] as
     readFT' a b ('\n':[])    = F ((read.reverse) a) ((read.reverse) b)
@@ -29,8 +30,12 @@ interactFT f s = do c <- interactS f s
 
 main :: IO ()
 main = do (file:args) <- getArgs
-          z <- interactFT file "Guess"
+          z <- interactFT file "paaaaaaa"
           putStr (show z)
+          --l <- lengthAttack file
+          --putStr (show l ++ "\n")
+          y <- sideChannel file
+          putStr (y ++ "\n")
           return ()
 
 ifCorrect :: FalseTuple Integer -> Bool
@@ -43,3 +48,48 @@ bruteForce = concat [bruteForce' n | n <- [1..]]
 bruteForce' :: Integer -> [String]
 bruteForce' 1 = [[x] | x <- givenArray]
 bruteForce' n = [(x:b) | x <- givenArray, b <- bruteForce' (n-1)]
+
+checkString :: FilePath -> String -> (IO String)
+checkString f s = do z <- interactFT f s
+                     if (ifCorrect z)
+                        then return s
+                        else return []
+
+attemptBruteForce :: FilePath -> IO String
+attemptBruteForce f = attemptBruteForce' f bruteForce
+
+attemptBruteForce' :: FilePath -> [String] -> IO String
+attemptBruteForce' f (x:xs) = do
+  putStr (x ++ ", ")
+  z <- interactFT f x
+  if (ifCorrect z)
+    then return x
+    else attemptBruteForce' f xs
+
+grabT :: FilePath -> String -> IO Integer
+grabT f s = do (F t r) <- interactFT f s
+               return t
+
+charByCharAttack :: FilePath -> Int -> IO String
+charByCharAttack = g "" 'a' where
+  g :: String -> Char -> FilePath -> Int -> IO String
+  g s c f n | n == (length s) = return s
+            | otherwise = do
+    time <- grabT f (s ++ (c:(take (n - (length s) - 1) ['a','a'..])))
+    if (time > (((1+).fromIntegral .length) s))
+      then g (s ++ [c]) 'a' f n
+      else g s ([c..] !! 1) f n
+
+lengthAttack :: FilePath -> IO Int
+lengthAttack = g 1 where
+  g :: Int -> FilePath -> IO Int
+  g n f = do time <- grabT f (take n ['a','a'..])
+             if (time > 0)
+               then return n
+               else g (n + 1) f
+
+sideChannel :: FilePath -> IO String
+sideChannel f = do
+  pwrdLen <- lengthAttack f
+  result <- charByCharAttack f pwrdLen
+  return result
